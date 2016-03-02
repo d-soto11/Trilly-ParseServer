184274bc5fec4aaac1c69e4f0ae75b385cfb77bb
Daniel Soto <d.soto11@uniandes.edu.co>
2016-03-02 10:31:10 -0500

    Try 2

--- cloud/main.js
+++ cloud/main.js
@@ -78,7 +78,7 @@ Parse.Cloud.afterSave("Ruta", function(request) {
 Parse.Cloud.beforeSave("Estadistica", function(request, response) {
     var km = request.object.get("kmRecorridos");
     var time = request.object.get("tiempo");
-    var KM_FOR_TREE = 200;
+    console.log("Constante!: "+KM_FOR_TREE);
     request.object.set("currentTree", ((km%KM_FOR_TREE)/KM_FOR_TREE)*100);
     request.object.set("savedTrees", km/KM_FOR_TREE);
     request.object.set("gas", km/45);
